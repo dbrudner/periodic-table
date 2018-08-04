d5845327b75fcc630b916a1c0d3e0246e22e7cc8
dbrudner <dbrudne1@gmail.com>
2018-08-04 14:29:15 -0500

    reverted

--- src/table.js
+++ src/table.js
@@ -8,9 +8,14 @@ const TableContainer = styled.div`
 	border: 1px solid black;
 `
 
-const Column = styled.div`
+const Row = styled.div`
 	display: flex;
-	flex-direction: column;
+	justify-content: space-between;
+`
+
+const Block = styled.div`
+	display: flex;
+	justify-content: space-between;
 `
 
 export default props => {
@@ -18,37 +23,48 @@ export default props => {
 	const twoRowElements = [...periodicTable.elements.slice(56, 70), ...periodicTable.elements.slice(87, 101)]
 	const elements = [...periodicTable.elements.slice(0, 55), ...periodicTable.elements.slice(71, 86), ...periodicTable.elements.slice(102)]
 
-	console.log(elements);
-	// The dopest function I've ever written
-	const columns = elements.reduce((acc, element, i) => {
-		const column = 15 - (15/Math.ceil(i + 15));
-		console.log(column);
-		if (!acc || !acc[column]) return acc;
-		acc[column].push(element);
-		return acc;
-	}, Array(15).fill([]))
-
-
-	const renderColumns = () => {
-		return columns.map(column => {
-			return (
-				<Column>
-					{column.map(element => <Element openDetailedView={props.openDetailedView} element={element} />)}
-				</Column>
-			)
-		})
-	}
-
 	const renderElementsBlock = elements => elements.map(element => <Element openDetailedView={props.openDetailedView} element={element} />)
 
 	return (
 		<div>
 			<TableContainer>
-				{renderColumns()}
+				<Row>
+					<Block>
+						<Element openDetailedView={props.openDetailedView} element={elements[0]} />
+					</Block>
+					<Block>
+						<Element openDetailedView={props.openDetailedView} element={elements[1]} />
+					</Block>
+				</Row>
+				<Row>
+					<Block>
+						{renderElementsBlock([...elements.slice(2,4)])}
+					</Block>
+					<Block>
+						{renderElementsBlock([...elements.slice(4,10)])}
+					</Block>
+				</Row>
+				<Row>
+					<Block>
+						{renderElementsBlock([...elements.slice(10,12)])}
+					</Block>
+					<Block>
+						{renderElementsBlock([...elements.slice(12,18)])}
+					</Block>
+				</Row>
+				<Row>
+					<Block>
+						{renderElementsBlock([...elements.slice(19,36)])}
+					</Block>
+				</Row>
+				<Row>
+					<Block>
+						{renderElementsBlock([...elements.slice(37,54)])}
+					</Block>
+				</Row>
 			</TableContainer>
 			{/* <div style={{marginTop: "50px"}}>
 				{twoRowElements.map(element => <Element twoRow element={element} />)}
 			</div> */}
 		</div>
-	)
-}
+)}
