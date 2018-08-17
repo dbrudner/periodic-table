ac5c705ad99c82a0f93f8e5b7fa849ee77058454
Brudner, Dave  - C <Brudner.Dave@principal.com>
2018-08-17 15:05:02 -0500

    resolving api stuff

--- src/compound-builder.js
+++ src/compound-builder.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default props => {
-	const { compound } = props;
+export default class extends Component {
+	state = { results: [] };
 
-	const renderCompoundName = () => {
-		return compound.map(element => {
+	search = async () => {
+		const compoundString = this.props.compound.reduce((cmpd, element) => {
+			const quantity = element.quantity > 1 ? element.quantity : "";
+			return cmpd + element.symbol + quantity;
+		}, "");
+		const url = `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/fastformula/${compoundString}/cids/json`;
+		const res = await fetch(url);
+		const data = await res.json();
+		// data.IdentifierList.CID.slice(10).forEach(async cid => {
+		// 	const url = `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/${cid}/classification/JSON?classification_type=original`;
+		// 	const res = await fetch(url);
+		// 	const data = await res.json();
+		// 	console.log(data);
+		// });
+
+		const urlel = await `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/${data.IdentifierList.CID[0]}/classification/JSON?classification_type=original`;
+		const resel = await fetch(url);
+		const datael = await res.json();
+		console.log(datael);
+	};
+
+	renderCompoundName = () => {
+		return this.props.compound.map(element => {
 			return (
-				<span>
+				<span key={element.symbol}>
 					{element.symbol}
 					<sub>{element.quantity > 1 && element.quantity}</sub>
 				</span>
@@ -14,18 +35,22 @@ export default props => {
 		});
 	};
 
-	const getMolarMass = () =>
-		compound
+	getMolarMass = () => {
+		return this.props.compound
 			.reduce((acc, element) => {
 				return acc + element.atomic_mass * element.quantity;
 			}, 0)
 			.toFixed(4);
+	};
 
-	return (
-		<div>
-			Compound
-			<div style={{ marginTop: "5px" }}>{renderCompoundName()}</div>
-			<div>{getMolarMass()}</div>
-		</div>
-	);
-};
+	render() {
+		return (
+			<div>
+				Compound
+				<div style={{ marginTop: "5px" }}>{this.renderCompoundName()}</div>
+				<div>{this.getMolarMass()}</div>
+				<button onClick={this.search}>Search</button>
+			</div>
+		);
+	}
+}
